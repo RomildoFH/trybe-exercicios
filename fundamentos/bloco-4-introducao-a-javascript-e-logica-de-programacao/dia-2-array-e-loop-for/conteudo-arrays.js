e92b263e67efc4c0d3de30b4113066503bc3a7c5
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-07-21 18:26:37 -0300

    Criando diretórios exercicio/4.2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/conteudo-arrays.js
@@ -0,0 +1,12 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+//Obtenha o valor "Serviços" do array menu:
+console.log(menu.indexOf("Serviços"));
+
+//Procure o índice do valor "Portfólio" do array menu:
+console.log(menu.indexOf("Portfólio"));
+
+//Adicione o valor "Contato" no final do array menu:
+menu.push("Contato");
+
+console.log(menu);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/script.js
@@ -0,0 +1,4 @@
+const jogos = ['Doom', 'Skyrim', 'Final Fantasy'];
+jogos[0] = 'Warcraft III';
+
+console.log(jogos);
