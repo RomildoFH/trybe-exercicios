3e6a62ebf4ede57d9454513652c125255aa8b04e
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-08-16 19:31:59 -0300

    Concluída questão 1

--- fundamentos/bloco-7-introducao-javascript-es6-testes-unitarios/dia-3-jest/exercicios/sum.js
+++ fundamentos/bloco-7-introducao-javascript-es6-testes-unitarios/dia-3-jest/exercicios/sum.js
@@ -1,4 +1,12 @@
 function sum(a, b) {
+    if(typeof a !== 'number' || typeof b !== 'number') {
+      throw new Error('parameters must be numbers');
+    }
     return a + b;
   }
+
+  console.log(sum(4, '5'));
+
   module.exports = sum;
+
+  
--- fundamentos/bloco-7-introducao-javascript-es6-testes-unitarios/dia-3-jest/exercicios/sum.test.js
+++ fundamentos/bloco-7-introducao-javascript-es6-testes-unitarios/dia-3-jest/exercicios/sum.test.js
@@ -1,6 +1,17 @@
-const { default: test } = require('node:test');
 const sum = require('./sum');
 
 test('Testar se sum(4, 5) return 9', () => {
     expect(sum(4, 5)).toBe(9);
+});
+
+test('Testar se sum(0, 0) return 0', () => {
+    expect(sum(0, 0)).toBe(0);
+});
+
+test("Testar se sum(4, '5') return erro", () => {
+    expect(() => { sum(4, '5') }).toThrow();
+});
+
+test("Testar se a mensagem de erro é 'parameters must be numbers'", () => {
+    expect(() => { sum(4, '5') }).toThrowError(new Error('parameters must be numbers'));
 });
