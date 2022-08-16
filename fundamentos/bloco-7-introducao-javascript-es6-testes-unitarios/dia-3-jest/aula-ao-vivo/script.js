fdde9ea11c7e04735ffdc20d576b18f4e6ba89e2
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-08-16 19:14:58 -0300

    Finalizado conteúdo das aulas

---
+++ fundamentos/bloco-7-introducao-javascript-es6-testes-unitarios/dia-3-jest/aula-ao-vivo/script.js
@@ -0,0 +1,20 @@
+// Exemplo 1
+function sum(a, b) {
+    return a + b;
+  }
+//   module.exports = sum;
+
+const summationOf = (number) => {
+    if (number === undefined) {
+        throw new Error('Number precisa ser um número real');
+    }
+    let summation = 0;
+    for (let index = 1; index <= number; index += 1) {
+        summation += index;
+    }
+    return summation;
+};
+
+console.log(summationOf(2));
+
+module.exports = {sum, summationOf};
