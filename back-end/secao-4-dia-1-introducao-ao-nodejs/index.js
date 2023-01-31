d540b5c02af247e8c1807bc8b21f4654da505a22
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-01-31 16:10:50 -0300

    Iniciando dia 4.2

--- back-end/secao-4-dia-1-introducao-ao-nodejs/index.js
+++ back-end/secao-4-dia-1-introducao-ao-nodejs/index.js
@@ -2,4 +2,8 @@ const path = require('path');
 
 console.log('Hello World!');
 
-console.log(path.dirname())
+console.log(path.dirname('/usr/share/gnome'));
+
+console.log(path.resolve('usr', 'share', 'gnome'));
+
+console.log(path.resolve('/', 'usr', 'share', 'gnome'));
