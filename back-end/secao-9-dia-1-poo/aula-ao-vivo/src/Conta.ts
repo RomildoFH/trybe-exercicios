08c8e91f33849fd7b88b0e231e691c380f0cef10
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-03-22 18:04:26 -0300

    Metodo pagamento implementado

--- back-end/secao-9-dia-1-poo/aula-ao-vivo/src/Conta.ts
+++ back-end/secao-9-dia-1-poo/aula-ao-vivo/src/Conta.ts
@@ -9,7 +9,18 @@ export default class Conta {
     return this._saldo;
   }
   
-  public set saldo(value: number) {
-    this._saldo = value;
+  public set saldo(valor: number) {
+    this._saldo = valor;
+  }
+
+  public debito(valor: number) {
+    try {
+      if (this._saldo < valor) {
+        throw new Error('Saldo Insuficiente');
+      }    
+      this._saldo -= valor;
+    } catch (error) {
+      console.log(String(error));
+    }
   }
 }
