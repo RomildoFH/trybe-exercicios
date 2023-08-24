17c3f627222820caa9da485de84962d00b2ec6b5
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-08-24 15:26:45 -0300

    finalizado exercicio-4

--- cs/dia3-2-arrays/exercicio/exercicio_4.py
+++ cs/dia3-2-arrays/exercicio/exercicio_4.py
@@ -13,3 +13,19 @@ resultado: 1
 O estudante 1 entrou no instante 1 e saiu no 3, já o segundo entrou
 e saiu no 2 e o último foi único a estar presente no instante 4.
 """
+
+
+def calcula_pessoas(entradas, saidas, instante):
+    pessoa_presente = []  # Espaço O(n)
+    for i in range(len(entradas)):  # Tempo O(n)
+        if i < len(saidas) and entradas[i] <= instante <= saidas[i]:
+            pessoa_presente.append(True)
+        else:
+            pessoa_presente.append(False)
+    return pessoa_presente.count(True)
+
+
+print(calcula_pessoas([1, 2, 3], [3, 2, 7], 4))
+print(calcula_pessoas([1, 2, 5], [3, 2, 7], 4))
+print(calcula_pessoas([1, 2, 3], [3, 5, 7], 4))
+print(calcula_pessoas([1, 2, 3], [4, 5, 7], 4))
